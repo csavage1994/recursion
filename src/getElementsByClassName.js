28e7594c19f55580792cb6712fb180ff887965b6
Colton Savage <csavage1994@gmail.com>
2016-01-11 13:54:50 -0600

    Initial commit for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className, node){
   // your code here
+  var matchedNodes = [];
+  var node = node || document.body;
+  // iterate thru all the nodes
+  for (var i = 0; i < className.children.length; i++) {
+    
+    // check nodes for classname
+    if(node.className === className){
+      matchedNodes.push(node);
+    }
+    //if node has children, iterate through children
+      //create var for recursive call to getElementsByClassName(classname, currentNode)
+      //push results into matchedNodes
+  }
+  
+  
+  return matchedNodes;
 };
